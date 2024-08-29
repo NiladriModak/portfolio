76618311c79275282b688da845a182ca36a51dee
NiladriModak <niladrimodakk2001@gmail.com>
2024-08-29 12:05:12 +0530

    resizing

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,9 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-400 selection:text-cyan-900">
-      <div className="absolute overflow-auto inset-0 -z-10 h-full w-full items-center  [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-        <div className="container mx-auto py-8 px-2 overflow-x-hidden">
+      <div className="fixed top-0 -z-10 h-full w-full"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full items-center overflow-x-hidden [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+        <div className="container mx-auto py-8 overflow-x-hidden">
           <Navbar />
           <Hero />
           <About />
