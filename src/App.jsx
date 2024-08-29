37cd285c13b894449cc03db3df3a2d0e79969924
NiladriModak <niladrimodakk2001@gmail.com>
2024-08-29 12:14:48 +0530

    white-patch

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,16 @@ import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-400 selection:text-cyan-900">
-      <div className="fixed top-0 -z-10 h-full w-full"></div>
-      <div className="absolute inset-0 -z-10 h-full w-full items-center overflow-x-hidden [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-        <div className="container mx-auto py-8 overflow-x-hidden">
-          <Navbar />
-          <Hero />
-          <About />
-          <Technologies />
-          <Achievements />
-          <Projects />
-          <Contact />
-        </div>
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-400 selection:text-cyan-900 min-h-screen">
+      <div className="fixed top-0 left-0 right-0 bottom-0 -z-10 min-h-screen w-full [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      <div className="container mx-auto py-8 overflow-x-hidden">
+        <Navbar />
+        <Hero />
+        <About />
+        <Technologies />
+        <Achievements />
+        <Projects />
+        <Contact />
       </div>
     </div>
   );
